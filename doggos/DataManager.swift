3a087d76798149bbc5a0d0499d1cb2f361d51f22
schen22 <sarahchen22@gmail.com>
2022-03-27 17:15:15 -0700

    add json parser example

--- doggos/DataManager.swift
+++ doggos/DataManager.swift
@@ -11,7 +11,6 @@ enum LocalDataManagerError: Error {
   case notFound
 }
 
-
 class LocalDataManager {
   var allData = [
     Day(
--- doggos/ViewModels/DayViewModel.swift
+++ doggos/ViewModels/DayViewModel.swift
@@ -5,8 +5,44 @@
 //  Created by Sarah Chen on 3/18/22.
 //
 
+import Foundation
+
 class DayViewModel {
 //  func retrieveDay(_ success: Callback) -> Day {
 //    return Day(title: "Day 1", thumbnail: UIImage(named: "flareon"))
 //  }
+
+  let contentJson = """
+    [
+      {
+        "id": "0",
+        "title": "3/10",
+        "thumbnail_name": "espeon",
+        "fuzzies": []
+      },
+      {
+        "id": "1",
+        "title": "3/8",
+        "thumbnail_name": "flareon",
+        "fuzzies": []
+      },
+      {
+        "id": "2",
+        "title": "2/18",
+        "thumbnail_name": "lantern",
+        "fuzzies": []
+      }
+    ]
+  """
+  
+  func parseDayJson() {
+    let storedData = Data(contentJson.utf8)
+    let decoder = JSONDecoder()
+    do {
+      let result = try decoder.decode([Day].self, from: storedData)
+      print(result)
+    } catch let error {
+      print("decoding error ocurred: \(error)")
+    }
+  }
 }
